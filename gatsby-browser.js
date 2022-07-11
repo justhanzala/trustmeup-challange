695016e2c09b71d365e36afdffc5b45133f694e6
hanzala <hanzala@nickelfox.co>
2022-07-11 19:40:42 +0530

    new redux change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,1 @@
-import React from "react";
-import { Provider } from "react-redux";
-import store from "./src/redux";
-
-export const wrapRootElement = ({ element }) => {
-  return <Provider store={store || {}}>{element}</Provider>;
-};
+export { default as wrapRootElement } from './src/redux/ReduxWrapper';
